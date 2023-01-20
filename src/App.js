cc6c876eb13320d4a8aee657633d7b61c076f513
Coding With Enjoy <codingwithenjoy@gmail.com>
2023-01-20 14:43:13 +0330

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./style.css";
+import { TiChevronLeftOutline, TiChevronRightOutline } from "react-icons/ti";
 
-function App() {
+const CARDS = 6;
+const MAX_VISIBILITY = 3;
+
+const Card = ({ title, content }) => (
+  <div className="card">
+    <h2>{title}</h2>
+    <p>{content}</p>
+  </div>
+);
+
+const Carousel = ({ children }) => {
+  const [active, setActive] = useState(2);
+  const count = React.Children.count(children);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="carousel">
+      {active > 0 && (
+        <button className="nav left" onClick={() => setActive((i) => i - 1)}>
+          <TiChevronLeftOutline />
+        </button>
+      )}
+      {React.Children.map(children, (child, i) => (
+        <div
+          className="card-container"
+          style={{
+            "--active": i === active ? 1 : 0,
+            "--offset": (active - i) / 3,
+            "--direction": Math.sign(active - i),
+            "--abs-offset": Math.abs(active - i) / 3,
+            "pointer-events": active === i ? "auto" : "none",
+            opacity: Math.abs(active - i) >= MAX_VISIBILITY ? "0" : "1",
+            display: Math.abs(active - i) > MAX_VISIBILITY ? "none" : "block",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {child}
+        </div>
+      ))}
+      {active < count - 1 && (
+        <button className="nav right" onClick={() => setActive((i) => i + 1)}>
+          <TiChevronRightOutline />
+        </button>
+      )}
     </div>
   );
-}
+};
+
+const App = () => (
+  <div className="app">
+    <Carousel>
+      {[...new Array(CARDS)].map((_, i) => (
+        <Card
+          title={"Card " + (i + 1)}
+          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        />
+      ))}
+    </Carousel>
+  </div>
+);
 
 export default App;
